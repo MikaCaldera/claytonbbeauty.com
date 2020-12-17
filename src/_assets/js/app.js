cdd476fad8f581e78a4439466d27787c2bd313fe
Mika Caldera <mika.caldera@gmail.com>
2020-12-17 01:34:13 -0500

    :construction_worker: Style and app edits

--- src/_assets/js/app.js
+++ src/_assets/js/app.js
@@ -1,9 +1,9 @@
 // JS Goes here - ES6 supported
 import $ from "jquery";
 
-// import 'bootstrap/js/dist/alert';
-// import 'bootstrap/js/dist/button';
-//import 'bootstrap/js/dist/carousel';
+import 'bootstrap/js/dist/alert';
+import 'bootstrap/js/dist/button';
+import 'bootstrap/js/dist/carousel';
 import 'bootstrap/js/dist/collapse';
 import 'bootstrap/js/dist/dropdown';
 // import 'bootstrap/js/dist/modal';
